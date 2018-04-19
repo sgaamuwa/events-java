5118c62a2f0cebf5adf56ab2bde16a2bcf24e87f
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-04-19 17:41:22 +0300

    Feature: Set up EventRepository to use JpaRepository methods

--- src/main/java/com/events/events/repository/EventRepository.java
+++ src/main/java/com/events/events/repository/EventRepository.java
@@ -1,8 +1,9 @@
 package com.events.events.repository;
 
+import com.events.events.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends JpaRepository {
+public interface EventRepository extends JpaRepository<Event, Integer> {
 }
