ccac08933e6fb7c56fb8572c03800f5ba307a24e
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-04-19 22:36:33 +0300

    Chore: Add the expection handler for the API

--- src/main/java/com/events/events/error/ResponseExceptionHandler.java
+++ src/main/java/com/events/events/error/ResponseExceptionHandler.java
@@ -1,4 +1,20 @@
 package com.events.events.error;
 
-public class ResponseExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {NotFoundException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException e, WebRequest request){
+        String bodyOfResponse = e.getMessage();
+        return handleExceptionInternal(e, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
 }
