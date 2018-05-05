1b15b1102f65a89c4f0e6092038a74ab2bd94c0c
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-05-05 14:42:54 +0300

    Task: Add Empty list exception - Create class for EmptyListException - Add handler method for the exception

--- src/main/java/com/events/events/error/EmptyListException.java
+++ src/main/java/com/events/events/error/EmptyListException.java
@@ -1,4 +1,18 @@
 package com.events.events.error;
 
-public class EmptyListException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NO_CONTENT)
+public class EmptyListException extends RuntimeException {
+
+    private String message;
+
+    public EmptyListException(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
 }
