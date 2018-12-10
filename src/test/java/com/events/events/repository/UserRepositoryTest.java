9f2493419148c031ba71efb7aeb7fa568e01ea06
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-12-10 15:34:52 +0300

    Chore: Remove redundant test

--- src/test/java/com/events/events/repository/UserRepositoryTest.java
+++ src/test/java/com/events/events/repository/UserRepositoryTest.java
@@ -37,33 +37,4 @@ public class UserRepositoryTest {
         Assert.assertEquals(samuel, returnedUser.get());
     }
 
-    @Test
-    public void testGetUserIdUsingFacebookIds(){
-        User samuel = new User("samuel", "gaamuwa", "sgaamuwa", "pass123", "sgaamuwa@email.com");
-        User edward = new User("edward", "gaamuwa", "egaamuwa", "pass123", "egaamuwa@email.com");
-        User david = new User("david", "gaamuwa", "dgaamuwa", "pass123", "dgaamuwa@email.com");
-        User merab = new User("merab", "kawanguzi", "mkawanguzi", "pass123", "mkawanguzi@email.com");
-
-        samuel.setFacebookId("12345");
-        edward.setFacebookId("21435");
-        david.setFacebookId("54321");
-        merab.setFacebookId("45231");
-
-        Integer samId = entityManager.persistAndGetId(samuel, Integer.class);
-        Integer edwardId = entityManager.persistAndGetId(edward, Integer.class);
-        Integer davidId = entityManager.persistAndGetId(david, Integer.class);
-        Integer merabId = entityManager.persistAndGetId(merab, Integer.class);
-        entityManager.flush();
-
-        List<Integer> singleId = Arrays.asList(samId);
-        List<Integer> multipleIds = Arrays.asList(samId, edwardId, davidId, merabId);
-
-        List<String> userIds = userRepository.getUserIdsForUsersWithFacebookIds(Arrays.asList("12345"));
-        List<String> userIds2 = userRepository.getUserIdsForUsersWithFacebookIds(Arrays.asList("12345", "21435", "54321", "45231"));
-
-        Assert.assertEquals(userIds.size(), 1);
-        Assert.assertEquals(userIds2.size(), 4);
-        Assert.assertEquals(new HashSet<>(singleId), new HashSet<>(userIds));
-        Assert.assertEquals(new HashSet<>(multipleIds), new HashSet<>(userIds2));
-    }
 }
