614372d3495b12f4f61daefd50d0fb329d9750da
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2019-01-23 17:41:14 +0300

    Feature: Add a bad request exception

--- src/main/java/com/events/events/error/BadRequestException.java
+++ src/main/java/com/events/events/error/BadRequestException.java
@@ -1,4 +1,19 @@
 package com.events.events.error;
 
-public class BadRequestException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    private String message;
+
+    public BadRequestException(String message){
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage(){
+        return message;
+    }
 }
